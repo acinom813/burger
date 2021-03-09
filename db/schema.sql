6ba8c69fbcfe3e6450019afdee2cda1ce80d7ae9
Monica Hill <monica.r.hill@gmail.com>
2021-03-09 14:59:18 -0500

    estabish db, config

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+--Drops the programming_db if it already exists--
+DROP DATABASE IF EXISTS burgers_db;
+
+--Create the DB "burgers_db"
+CREATE burgers_db;
+
+--Use the DB burgers_db for all the rest of the script
+USE burgers_db;
+
+--Created the table "burger" with 3 fields
+CREATE TABLE burger (
+    id int AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN,  
+    PRIMARY KEY (id)
+);
+
+
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+--Use the DB burgers_db for the rest of the script
+USE burgers_db;
+
+--Insert a set of records into the table
+INSERT INTO burger (burger_name, devoured) VALUES ('Blackbean Avocado', "false");
+INSERT INTO burger (burger_name, devoured) VALUES ('Beyond Meat Monster', "true");
+INSERT INTO burger (burger_name, devoured) VALUES ('Quinoa Burger', "true");
