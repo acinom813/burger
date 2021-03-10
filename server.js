2fffc34549aafd13601e458863ad43633e8323f9
Monica Hill <monica.r.hill@gmail.com>
2021-03-10 17:56:22 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express;
+
+//Serve static content for the app from the public directory
+app.use(express.static('public'));
+
+//Parse application body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Set handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.request('view engine', 'handlebars');
+
+//Import routes and give the server access
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+//Start server to begin listening to client requests
+app.listen(PORT, () =>
+ console.log(`server listening on: http://localhost:${PORT}`)
+);
